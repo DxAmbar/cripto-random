6b11461a467262cbb98d248e015a8cc8ea24cec5
DxAmbar <d.ambarbaeza@gmail.com>
2023-02-09 23:14:44 -0300

    Consume API y se muestra 'Cargando'

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { number } from "prop-types";
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const getRandomNumberFromApi = async (): Promise<number> => {
+  const res = await fetch(
+    "https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new"
+  );
+  const numberString = await res.text();
+  return +numberString;
+};
 
 export const App = () => {
-  const [count, setCount] = useState(0)
+  const [number, setNumber] = useState<number>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    getRandomNumberFromApi().then((num) => setNumber(num));
+  }, []);
+
+  useEffect(() => {
+    if (number) setIsLoading(false);
+  }, [number]);
 
   return (
     <div className="App App-header">
-      <h2>Número Aleatorio: </h2>
+      {isLoading ? <h2>Cargando...</h2> : <h2>Número Aleatorio: {number} </h2>}
     </div>
-  )
-}
-
-
+  );
+};
